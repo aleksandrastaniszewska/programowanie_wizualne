736a6a2ef8aae9f3db238cf6239fd005fbf95d1b
konieczkakacper <konieczkakacper9@gmail.com>
2022-10-24 18:56:13 +0200

    Dodaj pliki projektów.

---
+++ lab3/Form1.cs
@@ -0,0 +1,30 @@
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
