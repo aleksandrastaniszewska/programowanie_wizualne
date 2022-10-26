64a435a85fee4493f7b495cc41daac600c744032
Aleksandra Staniszewska <a.staniszewska287@gmail.com>
2022-10-26 18:45:37 +0200

    odczyt i zapis do pliku

--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,3 +1,17 @@
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
 namespace lab3
 {
     public partial class Form1 : Form
@@ -6,7 +20,8 @@ namespace lab3
         {
             InitializeComponent();
         }
-
+        string filePath = "C:\\Users\\olasx\\source\\repos\\programowanie_wizualne\\DataGridViewExport.csv";
+        string filePath2 = "C:\\Users\\olasx\\source\\repos\\programowanie_wizualne\\DataGridViewImport.csv";
         public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             
@@ -14,13 +29,22 @@ namespace lab3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            dataGridView1.SelectAll();
+            DataObject dataObject = dataGridView1.GetClipboardContent();
+            File.WriteAllText(filePath, dataObject.GetText(TextDataFormat.CommaSeparatedValue));
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            dataGridView1.Rows.Clear();
+            File.ReadLines(filePath2).Skip(1)
+                .Select(x => x.Split(','))
+                .ToList()
+                .ForEach(line => dataGridView1.Rows.Add(line.Where(e => !string.IsNullOrEmpty(e)).ToArray()));
         }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
